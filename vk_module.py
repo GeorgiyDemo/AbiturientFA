fe19822e8078e18d590b17b2811a0af006ad8ced
Georgiy Demenchuk <demenchuk.george@mail.ru>
2019-07-26 19:38:46 +0300

    Подключил поиск по группам финашки

--- vk_module.py
+++ vk_module.py
@@ -7,16 +7,18 @@ with open("./yaml/token.yaml", 'r') as stream:
 
 API_VERSION = 5.73
 USERS_YEARS = ["1999","2000", "2001","2002"]
+GROUPS_ID_LIST = [134724725, 6319]
 OUT_TXT_FILE = "OUTPUT.txt"
 
-def vk_search_good_output(vk_json):
+def vk_search_good_output(vk_json, groupflag):
     out_str = ""
     for item in vk_json:
 
         vk_link = "https://vk.com/id"+str(item["id"])
         obj = database_module.mysql_writer("INSERT INTO vk_users (first_name, last_name, link) VALUES ('"+item["first_name"]+"','"+item["last_name"]+"','"+vk_link+"')")
         if obj.result == True:
-            out_str += item["first_name"]+" "+item["last_name"]+" "+vk_link+"\n"
+            groupflag = (groupflag == True and " [по группе в вк]" or "" )
+            out_str += item["first_name"]+" "+item["last_name"]+" "+vk_link+groupflag+"\n"
 
     f = open(OUT_TXT_FILE, 'a')
     f.write(out_str)
@@ -36,22 +38,25 @@ class vk_processing():
     def input_processing(self):
         for item in self.result_array:
             print("*Работаем с пользователем "+item[3]+"*\nМесто "+str(item[1])+str(", балл ")+str(item[6])+" ["+str(item[5])+"]")
-            print(item[4]+"\n")
+            print(item[4])
             buf_name = item[3].split(" ")
-            self.search_profile_method(buf_name[0]+" "+buf_name[1])
-            time.sleep(5)
+            abit_name = buf_name[0]+" "+buf_name[1]
+            self.search_by_groups(abit_name)
+            time.sleep(3)
+            self.search_profile_method(abit_name)
+            time.sleep(3)
 
     def search_profile_method(self, inputname):
         year_processing_flag = True
 
         #Практически не помогает
-        print("[1] Простой поиск пользователя по VK")
+        print("***Простой поиск пользователя по VK***")
         simple_profiles = self.api.users.search(q=inputname,v=self.APIVersion)["items"]
-        if len(simple_profiles) > 5:
-            print("Ойй, много людей переезжаем на поиск по годам")
+        if len(simple_profiles) > 5 or simple_profiles == []:
+            print("Ойй, много людей переезжаем на поиск по годам..")
         else:
             print("Отлично, выборка достаточно малая")
-            print(vk_search_good_output(simple_profiles))
+            print(vk_search_good_output(simple_profiles, False))
             year_processing_flag = False
 
         if year_processing_flag == True:
@@ -59,21 +64,22 @@ class vk_processing():
             for born_year in USERS_YEARS:
                 time.sleep(1)
                 profiles = self.api.users.search(q=inputname,birth_year=born_year,v=self.APIVersion)["items"]
-                if (len(profiles) < 5):
+                if (len(profiles) < 5) or profiles != []:
                     print("Отлично, выборка достаточно малая по "+born_year+" году")
-                    print(vk_search_good_output(profiles))
+                    print(vk_search_good_output(profiles,False))
                     good_flag = True
                 else:
-                    print(born_year+" год, очень много профилей")
+                    print(born_year+" год, очень много профилей или их нет совсем((")
 
             if good_flag == True:
                 print("Кайф, возможно мы нашли Ч Е Л О В Е 4 К А")
 
-    #def search_news_def(self):
-    #    result = self.api.newsfeed.search(q='"'+string+'"',v=APIVersion)['items']
-
-
-
-
-
-
+    def search_by_groups(self, inputname):
+        """
+        Метод для поиска людей по группам Финашки
+        """
+        print ("Поиск по группам в вк..")
+        for group in GROUPS_ID_LIST:
+            group_results = self.api.users.search(q=inputname,group_id=group,v=self.APIVersion)["items"]
+            if group_results != []:
+                print(vk_search_good_output(group_results,True))
