9068494802ca49ce76db4af1aeab8123622cc88b
Georgiy Demo <demenchuk.george@protonmail.com>
2020-08-03 14:11:10 +0300

    Более адекватный модуль для MySQL
    Ужс какой-то был

--- SEARCH/database_module.py
+++ SEARCH/database_module.py
@@ -1,31 +1,53 @@
 import pymysql.cursors
 import yaml
 
-
-class mysql_writer():
-    def __init__(self, sql_string):
+class MySQLClass():
+    """Базовый класс"""
+    def __init__(self):
         with open("./yaml/DBlist.yaml", 'r') as stream:
-            self.DBLogin = yaml.safe_load(stream)
-        self.sql_string = sql_string
-        self.result = True
-        self.mysql_writer()
-
-    def mysql_writer(self):
-        """
-        Запись данных в БД
-        """
-        DBLogin = self.DBLogin
+            DBLogin = yaml.safe_load(stream)
         connection = pymysql.connect(host=DBLogin[0], port=DBLogin[1], user=DBLogin[2], password=DBLogin[3],
-                                     db=DBLogin[4], cursorclass=pymysql.cursors.DictCursor, autocommit=False)
-        try:
-            cursor = connection.cursor()
-            cursor.execute(self.sql_string)
-        except pymysql.err.IntegrityError:
-            self.result = False
-        except:
-            connection.rollback()
-            connection.close()
-            raise
-        else:
-            connection.commit()
-            connection.close()
+                db=DBLogin[4], cursorclass=pymysql.cursors.DictCursor)
+        self.connection = connection
+        self.cursor = connection.cursor()
+        self.result = None
+
+
+class MySQLWriter(MySQLClass):
+    """Запись данных в СУБД"""
+    def __init__(self, sql_string):
+        super().__init__()
+        self.sql_string = sql_string
+        self.processing()
+
+    def processing(self):
+        cursor = self.cursor
+        cursor.execute(self.sql_string)
+        self.connection.commit()
+        self.connection.close()
+
+class MySQLReaderOne(MySQLClass):
+    """Получение одного значения с СУБД"""
+    def __init__(self, sql_string):
+        super().__init__()
+        self.sql_string = sql_string
+        self.processing()
+
+    def processing(self):
+        cursor = self.cursor
+        cursor.execute(self.sql_string)
+        result = cursor.fetchone()
+        self.result = result
+
+class MySQLReaderAll(MySQLClass):
+    """Получение всех значение с СУБД"""
+    def __init__(self, sql_string):
+        super().__init__()
+        self.sql_string = sql_string
+        self.processing()
+
+    def processing(self):
+        cursor = self.cursor
+        cursor.execute(self.sql_string)
+        result = cursor.fetchall()
+        self.result = result
--- SEARCH/vk_module.py
+++ SEARCH/vk_module.py
@@ -15,23 +15,22 @@ GROUPS_ID_LIST = [134724725, 6319, 153039551, 27590309, 76552532, 196939200]
 OUT_TXT_FILE = "./OUTPUT/OUTPUT_VK.txt"
 
 
-def vk_search_good_output(vk_json, groupflag):
+def vk_search_good_output(vk_json, groupflag, full_name):
     out_str = ""
     for item in vk_json:
 
         vk_link = "https://vk.com/id" + str(item["id"])
         obj = database_module.mysql_writer(
-            "INSERT INTO vk_users (first_name, last_name, link) VALUES ('" + item["first_name"] + "','" + item[
-                "last_name"] + "','" + vk_link + "')")
-        if obj.result == True:
+            "INSERT INTO vk_users (first_name, last_name, link, full_name) VALUES ('{}','{}','{}','{}')".format(item["first_name"], item["last_name"], vk_link, full_name))
+        
+        if obj.result:
             groupflag = (groupflag == True and " [по группе в вк]" or "")
             out_str += item["first_name"] + " " + item["last_name"] + " " + vk_link + groupflag + "\n"
+            f = open(OUT_TXT_FILE, 'a')
+            f.write(out_str)
+            f.close()
 
-    f = open(OUT_TXT_FILE, 'a')
-    f.write(out_str)
-    f.close()
     return out_str
-
 
 class VkClass():
     def __init__(self, result_array):
@@ -47,17 +46,27 @@ class VkClass():
 
     def input_processing(self):
         for item in self.result_array:
-            print("**Работаем с пользователем " + item[3] + "**\nМесто " + str(item[1]) + str(", балл ") + str(
-                item[7]) + " [" + str(item[5]) + "]")
-            print(item[4])
-            buf_name = item[3].split(" ")
-            abit_name = buf_name[0] + " " + buf_name[1]
-            self.search_by_groups(abit_name)
-            time.sleep(3)
-            self.search_profile_method(abit_name)
-            time.sleep(3)
+            #Проверяем, есть ли результаты по нему в таблице
 
-    def search_profile_method(self, inputname):
+            obj = database_module.mysql_re("SELECT * FROM vk_users WHERE full_name='{}'".format(item[3]))
+            print(obj.result)
+            #TODO
+            break
+            if obj is None:
+
+                print("**Работаем с пользователем " + item[3] + "**\nМесто " + str(item[1]) + str(", балл ") + str(
+                    item[7]) + " [" + str(item[5]) + "]")
+                print(item[4])
+                buf_name = item[3].split(" ")
+                abit_name = buf_name[0] + " " + buf_name[1]
+                self.search_by_groups(abit_name, item[3])
+                time.sleep(3)
+                self.search_profile_method(abit_name, item[3])
+                time.sleep(3)
+
+
+
+    def search_profile_method(self, inputname, full_name):
         year_processing_flag = True
 
         # Практически не помогает
@@ -67,7 +76,7 @@ class VkClass():
             print("Ойй, много людей переезжаем на поиск по годам..")
         else:
             print("Отлично, выборка достаточно малая")
-            print(vk_search_good_output(simple_profiles, False))
+            print(vk_search_good_output(simple_profiles, False, full_name))
             year_processing_flag = False
 
         if year_processing_flag == True:
@@ -77,7 +86,7 @@ class VkClass():
                 profiles = self.api.users.search(q=inputname, birth_year=born_year, v=self.APIVersion)["items"]
                 if (len(profiles) < 5) and profiles != []:
                     print("Отлично, выборка достаточно малая по " + born_year + " году")
-                    print(vk_search_good_output(profiles, False))
+                    print(vk_search_good_output(profiles, False, full_name))
                     good_flag = True
                 else:
                     print(born_year + " год, очень много профилей или их нет совсем((")
@@ -85,7 +94,7 @@ class VkClass():
             if good_flag == True:
                 print("Кайф, возможно мы нашли Ч Е Л О В Е 4 К А")
 
-    def search_by_groups(self, inputname):
+    def search_by_groups(self, inputname, full_name):
         """
         Метод для поиска людей по группам Финашки
         """
@@ -93,4 +102,4 @@ class VkClass():
         for group in GROUPS_ID_LIST:
             group_results = self.api.users.search(q=inputname, group_id=group, v=self.APIVersion)["items"]
             if group_results != []:
-                print(vk_search_good_output(group_results, True))
+                print(vk_search_good_output(group_results, True, full_name))
